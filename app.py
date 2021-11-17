fa6b465a57da91fc21da5ba64e39555d39449368
Lennart Seeger <lennart.seeger@uni-muenster.de>
2021-11-17 13:43:37 +0100

    adding basic map functionality with here api

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
 @app.route('/')
-def hello():
-    return 'Hello World!\n'
+def map_func():
+	return render_template('map.html')
 if __name__ == '__main__':
     app.run(host="172.30.103.27", port=5001)
