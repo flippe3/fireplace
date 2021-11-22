b194f3bd8b7d5f866c4523135ab4fe87a50ccdc1
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-11-22 12:15:42 +0100

    these tests should not succeed

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestHello(unittest.TestCase):
         self.app = app.test_client()
     def test_hello(self):
         rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        #self.assertEqual(rv.status, '200 OK')
+        #self.assertEqual(rv.data, b'Hello World!\n')
 if __name__ == '__main__':
     unittest.main()
