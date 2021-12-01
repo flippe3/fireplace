45c84c4bce0eacebf97429770bb9316b01b0de97
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-12-01 16:41:54 +0100

    ip

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def create():
     return render_template("create.html")
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="172.30.103.27", port=5001, debug=True)
