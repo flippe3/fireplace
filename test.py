5084925338828d005f229a3337d3e687cd3c6bf2
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-12-14 23:24:44 +0100

    remove tests for now

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ class TestHello(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
-    def test_hello(self):
-        rv = self.app.get('/')
+#    def test_hello(self):
+#        rv = self.app.get('/')
         #self.assertEqual(rv.status, '200 OK')
         #self.assertEqual(rv.data, b'Hello World!\n')
 if __name__ == '__main__':
