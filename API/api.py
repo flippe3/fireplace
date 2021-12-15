4cde28049f0dceffcfc86c7cc4b85fa1f9bddfcc
Lennart Seeger <lennart.seeger@uni-muenster.de>
2021-12-15 17:29:01 +0100

    bugfix debugging

--- API/api.py
+++ API/api.py
@@ -84,7 +84,7 @@ def upload_file():
     mydb = connect_db()
     cursor = mydb.cursor()
     cursor.execute("USE firedb")
-    cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
+    #cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
     mydb.commit()
     mydb.commit()
     return "", 204
@@ -125,7 +125,7 @@ def create():
     else:
         wood = "FALSE"
     cursor.execute("USE firedb;")
-    cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
+    #cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
     mydb.commit()
     cursor.execute(
         "INSERT INTO fireplaces (name, latitude, longitude, wood) VALUES (\"" + str(name) + "\", " + str(
@@ -142,7 +142,7 @@ def delete():
 
     id = request.args.get('id')
     cursor.execute("USE firedb")
-    cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
+    #cursor.execute("INSERT INTO debugger(message) VALUES(\"" + str(request.args.get('token')) + "\");")
     mydb.commit()
     cursor.execute("DELETE FROM fireplaces WHERE id =\"" + id + "\";")
     mydb.commit()
